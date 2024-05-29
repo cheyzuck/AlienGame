5813f80abae523d96f878b3495da43646c5890ac
Cheyenne <chzuck@my365.bellevue.edu>
2024-05-29 18:23:06 -0400

    bullet limits

--- aliengame/alieninvasion.py
+++ aliengame/alieninvasion.py
@@ -26,6 +26,10 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
 
     def _check_events(self):
@@ -56,7 +60,16 @@ class AlienInvasion:
     def _fire_bullet(self):
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
+        if len(self.bullets) < (self.settings.bullets_allowed):
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
     
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
